2fe485f6d68d9e7d1cafcbaac86502a023de5351
SayuNirappel <sayunirappel@gmail.com>
2025-03-07 11:23:42 +0530

    first commit

---
+++ lib/view/quiz_screen/quiz_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {}
+
+class QuizScreen extends StatelessWidget {
+  const QuizScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(),
+      ),
+    );
+  }
+}
