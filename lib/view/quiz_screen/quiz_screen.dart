ec97c7ee0321e2c09b6a2548af7e304b1ee77d20
SayuNirappel <sayunirappel@gmail.com>
2025-03-07 11:46:17 +0530

    quizx page ui completed

--- lib/view/quiz_screen/quiz_screen.dart
+++ lib/view/quiz_screen/quiz_screen.dart
@@ -8,9 +8,110 @@ class QuizScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Padding(
         padding: EdgeInsets.all(20),
-        child: Column(),
+        child: Column(
+          spacing: 20,
+          children: [
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Text(
+                  "data",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(width: 3, color: Colors.white),
+                  borderRadius: BorderRadius.circular(10)),
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Berlin",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Icon(
+                      Icons.circle_outlined,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(width: 3, color: Colors.white),
+                  borderRadius: BorderRadius.circular(10)),
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Berlin",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Icon(
+                      Icons.circle_outlined,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(width: 3, color: Colors.white),
+                  borderRadius: BorderRadius.circular(10)),
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Berlin",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Icon(
+                      Icons.circle_outlined,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(width: 3, color: Colors.white),
+                  borderRadius: BorderRadius.circular(10)),
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Berlin",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Icon(
+                      Icons.circle_outlined,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
